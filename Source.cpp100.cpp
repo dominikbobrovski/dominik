fa51f2446988d817bf0ec57afd064841422ae1b8
dominikbobrovski <91685863+dominikbobrovski@users.noreply.github.com>
2021-11-18 17:06:59 +0300

    sm dominik masiv

---
+++ Source.cpp100.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include <ctime>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	const int n = 12;
+	int arr[n];
+	int temp;
+	srand(time(0));
+	for (int i = 0; i < n; i++) arr[i] = rand() % 101 - 50;
+	for (int i = 0; i < n; i++) cout << arr[i] << ' ';
+	cout << endl;
+
+	for (int i = 0; i < n; i++) {
+		int min = arr[i];
+		int p = i;
+		for (int j = i; j < n; j++) {
+			if (arr[j] < min) {
+				min = arr[j];
+				p = j;
+			}
+		}
+		swap(arr[i], arr[p]);
+	}
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+
+
+
+}
