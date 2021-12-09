5eb7d328c62b9ab1520b89fb4e86be8aa7ba62f4
dominikbobrovski <91685863+dominikbobrovski@users.noreply.github.com>
2021-12-09 18:39:14 +0300

    dominik

---
+++ Knight.h
@@ -0,0 +1,36 @@
+//knight
+#include <iostream>
+#include <ctime>
+using namespace std;
+class knight {
+private:
+	string name;
+	int hp;
+	int damage;
+	int armor;
+	int critical;
+public:
+	knight(string  fname, int fhp, int fdamage, int farmor, int fcritical) {
+		name = fname;
+		hp = fhp;
+		damage = fdamage;
+		armor = farmor;
+		critical = fcritical;
+	}
+	~knight() {
+		cout << "knight destroyed\n";
+	}
+	string getname() { return name; }
+	int gethp() { return hp; }
+	int getdamage() { return damage; }
+	int getarmor() { return armor; }
+	int getcritical() { return critical; }
+	void setname(string fname) { name = fname; }
+	void sethp(int fhp) { hp = fhp; }
+	void setdamage(int fdamage) { damage = fdamage; }
+	void  setarmor(int farmor) { armor = farmor; }
+	void setcritical(int fcritical) { critical = fcritical; }
+	void printKnight() {
+		cout << "Knight" << ' ' << name << ' ' << "has" << ' ' << hp << ' ' << "hp\n";
+	}
+};
